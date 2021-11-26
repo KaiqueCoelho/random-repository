ebb3706f2a39589d29ad8a907e5f93e5487c20f1
Kaique Coelho <kaiquescoelho@hotmail.com>
2021-11-26 13:59:06 -0300

    adiciona fixando aprendizado bloco 4 dia 1

---
+++ fundamentos/bloco-04-javascript-e-logica-de-programação/dia-01-javascript-primeiros-passos/fixando-aprendizado/script.js
@@ -0,0 +1,8 @@
+let myName = "Kaique";
+const birthCity = "São Paulo";
+let birthYear = 1996;
+birthYear = 2030;
+birthCity = "Fortaleza"
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
