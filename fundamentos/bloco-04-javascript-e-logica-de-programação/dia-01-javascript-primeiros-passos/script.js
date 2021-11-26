764546fbda48fe0cb7f3ee225bfa4f1f97d1a349
Kaique Coelho <kaiquescoelho@hotmail.com>
2021-11-26 14:00:52 -0300

    remove arquivo vazio

--- fundamentos/bloco-04-javascript-e-logica-de-programação/dia-01-javascript-primeiros-passos/script.js
+++
@@ -1,8 +0,0 @@
-let myName = "Kaique";
-const birthCity = "São Paulo";
-let birthYear = 1996;
-birthYear = 2030;
-birthCity = "Fortaleza"
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
