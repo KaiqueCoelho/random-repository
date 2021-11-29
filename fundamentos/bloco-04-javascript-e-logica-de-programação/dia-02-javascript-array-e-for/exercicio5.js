2d54b265d19f390c947a436cd367ccd514932946
Kaique Coelho <kaiquescoelho@hotmail.com>
2021-11-29 17:46:44 -0300

    otimiza codigo dos exercicios

--- fundamentos/bloco-04-javascript-e-logica-de-programação/dia-02-javascript-array-e-for/exercicio5.js
+++ fundamentos/bloco-04-javascript-e-logica-de-programação/dia-02-javascript-array-e-for/exercicio5.js
@@ -1,11 +1,8 @@
-let numbers = [500, 9, 3, 19, 70, 8, 100, 200, 35, 27];
+let numbers = [500, 509, 3, 19, 70, 8, 100, 200, 35, 27];
 let maiorNumero = 0;
 for (let index = 0; index < numbers.length; index+=1) {
     if (numbers[index] > maiorNumero) {
         maiorNumero = numbers[index]
-    }
-    else {
-        maiorNumero = maiorNumero
     }
 }
 
--- fundamentos/bloco-04-javascript-e-logica-de-programação/dia-02-javascript-array-e-for/exercicio7.js
+++ fundamentos/bloco-04-javascript-e-logica-de-programação/dia-02-javascript-array-e-for/exercicio7.js
@@ -1,11 +1,8 @@
-let numbers = [500, 9, 3, 19, 70, -8, 100, 200, 35, 27];
+let numbers = [500, 9, 3, -99, 70, -87, 100, 200, 35, 27];
 let menorNumero = 0;
 for (let index = 0; index < numbers.length; index+=1) {
     if (numbers[index] < menorNumero) {
         menorNumero = numbers[index]
-    }
-    else {
-        menorNumero = menorNumero
     }
 }
 
