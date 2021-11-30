9191245ce49752a78ec60a47b24a8cdc9bdf1adf
Kaique Coelho <kaiquescoelho@hotmail.com>
2021-11-30 18:16:16 -0300

    adiciona exercicio 1 completo 4/3

---
+++ fundamentos/bloco-04-javascript-e-logica-de-programação/dia-03-javascript-logica-e-algoritimos/exercicio.js
@@ -0,0 +1,8 @@
+let coluna = 20;
+let linha = "";
+for (let index = 0; index < coluna; index += 1) {
+  linha = linha + "*";
+}
+for (let index = 0; index < coluna; index +=1){
+  console.log(linha)
+}
