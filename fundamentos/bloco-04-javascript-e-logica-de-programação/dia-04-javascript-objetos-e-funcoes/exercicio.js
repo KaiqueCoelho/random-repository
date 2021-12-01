a42b18c9e544bee9e2b312ae59d63b139c680766
Kaique Coelho <kaiquescoelho@hotmail.com>
2021-12-01 18:42:01 -0300

    adiciona exercicios parte 1 4/4

---
+++ fundamentos/bloco-04-javascript-e-logica-de-programação/dia-04-javascript-objetos-e-funcoes/exercicio.js
@@ -0,0 +1,23 @@
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim",
+};
+console.log("Bem vinda,", info["personagem"]);
+info["recorrente"] = "Sim";
+console.log(info);
+
+for (key in info) {
+  console.log(key);
+}
+
+for (valor in info) {
+  console.log(info[valor]);
+}
