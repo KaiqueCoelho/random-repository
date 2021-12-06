34b444dfebaf5d9d577f70a7c6717be6819734a8
Kaique Coelho <kaiquescoelho@hotmail.com>
2021-12-06 15:37:53 -0300

    adiciona primeiro exercicio do fixando aprendizados

---
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-02-javascript-elementos/fixando-aprendizados/script.js
@@ -0,0 +1,13 @@
+document.querySelector('#elementoOndeVoceEsta')
+
+document.querySelector('#elementoOndeVoceEsta').parentElement.style.color = 'green';
+
+document.querySelector('#elementoOndeVoceEsta').firstElementChild.innerText = 'teste';
+
+document.querySelector('#pai').firstElementChild
+
+document.querySelector('#elementoOndeVoceEsta').parentElement.firstElementChild
+
+document.querySelector('#elementoOndeVoceEsta').parentElement.childNodes[4]
+
+document.querySelector('#pai').childNodes[5]
