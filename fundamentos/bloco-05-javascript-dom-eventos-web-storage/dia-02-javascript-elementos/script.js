c9af8d086ea865c50eae74114916b2c306f29c3c
Kaique Coelho <kaiquescoelho@hotmail.com>
2021-12-07 00:41:07 -0300

    adiciona exercicios 3 4 5 6 7 8 completos

--- fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-02-javascript-elementos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-02-javascript-elementos/script.js
@@ -5,4 +5,47 @@ body.appendChild(h1);
 
 let main = document.createElement('main')
 body.appendChild(main);
-main.className = 'main-content';
+main.className = 'main-content';   // adiciona tag main com classe main content
+
+let section = document.createElement('section');
+main.appendChild(section);
+section.className = 'center-content';
+
+let p = document.createElement('p');
+section.appendChild(p);
+p.innerText = 'Texto qualquer';
+
+let sectionLeft = document.createElement('section');
+main.appendChild(sectionLeft);
+sectionLeft.className = 'left-content';
+
+let sectionRight = document.createElement('section');
+main.appendChild(sectionRight);
+sectionRight.className = 'right-content';
+
+let img = document.createElement('img');
+sectionLeft.appendChild(img);
+img.src = "https://picsum.photos/200";
+img.className = 'small-image';
+
+let ul = document.createElement('ul');
+sectionRight.appendChild(ul);
+
+
+
+for(let index = 0; index < 10; index += 1) {
+    let li = document.createElement('li')
+    ul.appendChild(li)[index];
+}
+
+ul.children[0].innerText = 'Um';
+ul.children[1].innerText = 'Dois';
+ul.children[2].innerText = 'Três';
+ul.children[3].innerText = 'Quatro';
+ul.children[4].innerText = 'Cinco';
+ul.children[5].innerText = 'Seis';
+ul.children[6].innerText = 'Sete';
+ul.children[7].innerText = 'Oito';
+ul.children[8].innerText = 'Nove';
+ul.children[9].innerText = 'Dez';
+
