38ac2f55bc1f4136232337c304d651593d20defb
Kaique Coelho <kaiquescoelho@hotmail.com>
2021-12-07 00:14:59 -0300

    adiciona exercicios 1 e 2 completos

---
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-02-javascript-elementos/script.js
@@ -0,0 +1,8 @@
+let body = document.querySelector('body');
+let h1 = document.createElement('h1');
+h1.innerText = 'Exercício 5.2 - JavaScript DOM';  // cria titulo h1
+body.appendChild(h1);
+
+let main = document.createElement('main')
+body.appendChild(main);
+main.className = 'main-content';
