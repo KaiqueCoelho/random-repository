0b4bc907e780fcf4012a38879b89a9caa2f99bb4
Kaique Coelho <kaiquescoelho@hotmail.com>
2021-12-06 23:44:30 -0300

    adiciona exercicios fixando aprendizado

--- fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-02-javascript-elementos/fixando-aprendizados/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-02-javascript-elementos/fixando-aprendizados/script.js
@@ -11,3 +11,20 @@ document.querySelector('#elementoOndeVoceEsta').parentElement.firstElementChild
 document.querySelector('#elementoOndeVoceEsta').parentElement.childNodes[4]
 
 document.querySelector('#pai').childNodes[5]
+
+let local = document.getElementById('pai');
+local.appendChild(document.createElement('div')) // cria div irmã do elementoOndeVoceEsta
+
+let local2 = document.getElementById('elementoOndeVoceEsta');
+local2.appendChild(document.createElement('div')) // cria div filha do elementoOndeVoceEsta
+
+let local3 = document.getElementById('primeiroFilhoDoFilho');
+local3.appendChild(document.createElement('div')) // cria div filha do primeiroFilhoDoFilho
+
+let div = document.getElementById('primeiroFilhoDoFilho').lastChild
+div.parentElement.parentElement.nextElementSibling // acessa terceiro filho
+
+document.querySelectorAll('section')[4].remove();
+document.querySelectorAll('section')[4].remove();
+document.querySelectorAll('section')[4].remove();
+
