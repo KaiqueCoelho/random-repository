f034c021fed372b1ec8464c49edfdbfda53da745
Kaique Coelho <kaiquescoelho@hotmail.com>
2021-12-09 10:17:02 -0300

    adiciona local storage no exercicio 5/4

--- fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-04-javascript-web-storage/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-04-javascript-web-storage/script.js
@@ -6,21 +6,33 @@ const tipoLetra = document.querySelector('#ftType');
 
 corLetra.addEventListener('click', function(){
     document.querySelector('p').classList.toggle('blue')
+    localStorage.setItem('class', document.querySelector('p').className)
 })
 
 corFundo.addEventListener('click', function(){
     document.querySelector('section').classList.toggle('wheat')
+    localStorage.setItem('backgroundColor', document.querySelector('section').className)
 })
 
 tamanhoLetra.addEventListener('click', function() {
     document.querySelector('p').classList.toggle('grande')
+    localStorage.setItem('class', document.querySelector('p').className)
 })
 
 espacamento.addEventListener('click', function() {
     document.querySelector('p').classList.toggle('espacamento')
+    localStorage.setItem('class', document.querySelector('p').className)
 })
 
 tipoLetra.addEventListener('click', function() {
     document.querySelector('p').classList.toggle('sans')
+    localStorage.setItem('class', document.querySelector('p').className)
 })
+
+window.onload = carregaItens;
+
+function carregaItens () {
+    document.querySelector('p').className = localStorage.getItem('class');
+    document.querySelector('section').className = localStorage.getItem('backgroundColor');
+}
 
